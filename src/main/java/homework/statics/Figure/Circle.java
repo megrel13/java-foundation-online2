8cf85867b615f9a06fe01e73b0a255db753e05c4
megrel13 <g-o-g-i-@mail.ru>
2022-01-26 15:40:37 +0300

    add abstract class Figure, Circle, FigureMain

---
+++ src/main/java/homework/statics/Figure/Circle.java
@@ -0,0 +1,9 @@
+package homework.statics.Figure;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor@Data
+public class Circle extends Figure{
+
+}
---
+++ src/main/java/homework/statics/Figure/FigureMain.java
@@ -0,0 +1,10 @@
+package homework.statics.Figure;
+
+public class FigureMain {
+    public static void main(String[] args) {
+        Figure circle = new Circle();
+
+        System.out.println("Figure.PI = " + Figure.PI);
+        System.out.println("Circle.PI = " + Circle.PI);
+    }
+}
