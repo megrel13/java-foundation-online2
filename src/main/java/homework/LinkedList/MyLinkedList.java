a4d832b61cccb986af23c887ed16fb3e8b6a5ab3
megrel13 <g-o-g-i-@mail.ru>
2022-04-29 10:02:33 +0300

    add 3 new methods in LinkedList

--- src/main/java/homework/LinkedList/MyLinkedList.java
+++ src/main/java/homework/LinkedList/MyLinkedList.java
@@ -46,4 +46,78 @@ public class MyLinkedList {
         }
         return true;
     }
+
+    public boolean remove(Object o) {
+        if (head == null) return false;// если пустой список, ничего не возвращаем
+
+        if (head.getValue().equals(o)) { // если совпал элемент в head, переместить указатель на след элемент
+            head = head.getNext();
+            return true;
+        }
+        if (head.getNext() == null) return false; // Провепяем есть ли дальше объект за head
+
+        Node curNode = head;
+        Node prevNode = head;
+
+        while ((curNode = curNode.getNext()) != null) { // проверяем в curNode есть элемент или нет
+            if (curNode.getValue().equals(o)) { // есди нет перемещаем curNode и prevNode дальше
+                break;
+            }
+            prevNode = prevNode.getNext();
+        }
+
+        if (curNode == null) return false; // проверка curNode на null
+
+        prevNode.setNext(curNode.getNext()); // если нашли элемент, делаем prevNode setNext
+        curNode.setNext(null);// убираем ссылку
+        return true;
+    }
+
+    public void clear() {
+        head = null;
+    }
+
+    public Object get(int index) {
+
+
+        if (isEmpty()) {
+            return null;
+        }
+        if (index < 0 || index > size()) {
+            return null;
+        }
+        int i = 0;
+        Node curNode = head;
+        while (curNode != null) {
+
+            if (i == index) {
+                return curNode.getValue();
+            }
+            curNode = curNode.getNext();
+            i++;
+        }
+        return null;
+    }
+
+    public Object set(int index, Object element) {
+        if (!isCorrectIndex(index)) {
+            throw new ArrayIndexOutOfBoundsException("Некорректный индекс");
+        }
+        Node curNode = head;
+        int i = 0;
+        while (i < index) {
+            curNode = curNode.getNext();
+            i++;
+        }
+        Object prevNode = curNode.getValue();
+        curNode.setValue(element);
+        return prevNode;
+    }
+
+    private boolean isCorrectIndex(int index) {
+        if (index > -1 && index < size()) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/homework/LinkedList/MyLinkedListPractice.java
+++ src/main/java/homework/LinkedList/MyLinkedListPractice.java
@@ -8,9 +8,21 @@ public class MyLinkedListPractice {
         list.add("Строка2");
         System.out.println("list = " + list);
         list.add("Строка3");
+
         System.out.println("list = " + list);
         System.out.println("list.size() = " + list.size());
         System.out.println("list.isEmpty() = " + list.isEmpty());
         System.out.println("list.contains(\"Строка4\") = " + list.contains("Строка4"));
+
+
+        list.remove(1);
+        System.out.println("list = " + list);
+        System.out.println("list.get(0) = " + list.get(0));
+
+        list.set(0, "Строка0");
+        System.out.println("list = " + list);
+
+
     }
+    
 }
