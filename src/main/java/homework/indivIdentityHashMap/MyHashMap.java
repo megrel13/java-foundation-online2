6a40226acc7ba0d94cf1de843a09f602030376f7
megrel13 <g-o-g-i-@mail.ru>
2022-05-06 09:19:06 +0300

    IteratorHW1

--- src/main/java/homework/indivIdentityHashMap/MyHashMap.java
+++ src/main/java/homework/indivIdentityHashMap/MyHashMap.java
@@ -1,20 +1,11 @@
 package homework.indivIdentityHashMap;
 
+import java.util.HashSet;
+
 public interface MyHashMap {
 
-    // размер
-    int size();
-
-    // пусто
-    boolean isEmpty();
-
-    // Получить элементы на основе ключа
-    Object get(Object key);
-
-    // Добавить элементы
     Object put(Object key, Object value);
-
-    // Внутренний интерфейс
+    
     interface Entry<k, v> {
         k getKey();
 
